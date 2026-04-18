7ca2681c43657fc48d7276c08d66f4a31469ca80
akashjainn <akashjain1311@gmail.com>
2026-04-18 00:32:53 -0400

    shifted header logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,15 +29,15 @@ export default function HomePage() {
         {/* Brand row: STARLINK wordmark + logo mark */}
         <div
           className="brand-row anim-text"
-          style={{ position: "absolute", top: 50, left: 90, display: "flex", alignItems: "center", gap: 14, lineHeight: 1 }}
+          style={{ position: "absolute", top: 60, left: 90 }}
         >
           <span className="wordmark">STARLINK</span>
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
+            className="logo-mark logo-mark--hero"
             src="/starlink-mark-52.svg"
             alt=""
             aria-hidden="true"
-            style={{ width: 52, height: 52, flexShrink: 0, display: "block", objectFit: "contain" }}
           />
         </div>
 
